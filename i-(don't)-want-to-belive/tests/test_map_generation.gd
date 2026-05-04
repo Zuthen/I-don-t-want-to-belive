86764d78af9fc36686e795a894428149ece1a274
Zuthen <winstonivo@gmail.com>
2026-05-04 18:59:44 +0200

    directions test

--- i-(don't)-want-to-belive/tests/test_map_generation.gd
+++ i-(don't)-want-to-belive/tests/test_map_generation.gd
@@ -1,12 +1,27 @@
 extends GutTest
 
-
-
 func test_bush_created():
+	# Arrange
 	var game: PackedScene = preload("uid://c4twc836ak4bd")
 	var test_game = autoqfree(game.instantiate())
 	add_child(test_game)
+	# Act
 	test_game.create_bush(Vector2i.ZERO)
 	var obstacles = test_game.paths
+#	Assert
 	assert_true(!obstacles.is_empty())
-	test_game.queue_free()
+
+func test_directions():
+#	Arrange
+	var game: PackedScene = preload("uid://c4twc836ak4bd")
+	var test_game = autoqfree(game.instantiate())
+	var expected: Dictionary = {
+		"up": Vector2i(0, -8),
+		"down": Vector2i(0, 8),
+		"left": Vector2i(-8, 0),
+		"right":Vector2i(8, 0)
+	}
+# 	Act
+	var sut = test_game.directions(8)
+# Assert
+	assert_eq(sut, expected)
