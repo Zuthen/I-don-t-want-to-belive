7f1dedb1be703c63c4511a73403f1e163f2ef584
Zuthen <winstonivo@gmail.com>
2026-05-24 23:46:49 +0200

    unflaking tests?

--- i-(don't)-want-to-belive/tests/test_call_skeptic.gd
+++ i-(don't)-want-to-belive/tests/test_call_skeptic.gd
@@ -10,35 +10,46 @@ func before_each():
 
 
 func after_each():
+	# Czyścimy TileMapLayer z Godota 4, o którym rozmawialiśmy,
+	# aby całkowicie zresetować fizykę kafelków między testami
+	if is_instance_valid(game):
+		for child in game.get_children():
+			if child is TileMapLayer:
+				child.update_internals()
+				child.clear()
 	game = null
-	await wait_physics_frames(2)
 
 
 func test_player_can_call_other_player():
 	# Arrange
-	var player = autoqfree(game.spawn_player(Vector2i(0, 0)))
-	var second_player = autoqfree(game.spawn_player(Vector2i(2, 2)))
+	var player = game.spawn_player(Vector2i(0, 0))
+	game.spawn_player(Vector2i(2, 2))
 
 	# Act
 	player.call_other_skeptic()
-	await wait_physics_frames(3)
+
+	var icons = []
+	var attempts = 0
+	while icons.size() == 0 and attempts < 10:
+		await wait_physics_frames(1)
+		icons = find_all_icons_in_engine(get_tree().root)
+		attempts += 1
 
 	# Assert
-	var icons = find_all_icons_in_engine(get_tree().root)
-	for icon in icons:
-		autoqfree(icon)
-
 	assert_gt(icons.size(), 0)
 
 
 func test_player_can_t_call_outside_range_size():
 	# Arrange
-	var player = autoqfree(game.spawn_player(Vector2i(0, 0)))
-	var second_player = autoqfree(game.spawn_player(Vector2i(10, 10)))
+	var player = game.spawn_player(Vector2i(0, 0))
+	game.spawn_player(Vector2i(10, 10))
 
 	# Act
 	player.call_other_skeptic()
-	await wait_physics_frames(3)
+
+	# W teście negatywnym czekamy stałe 5 klatek fizyki,
+	# dając silnikowi pełną szansę na ewentualne błędne wykrycie kolizji.
+	await wait_physics_frames(5)
 
 	# Assert
 	var icons = find_all_icons_in_engine(get_tree().root)
