78a8044ce6c7a93fe2de9714acea08e375117a64
Zuthen <winstonivo@gmail.com>
2026-06-02 23:42:50 +0200

    coordinates code clean

--- i-(don't)-want-to-belive/scenes/ui/coordinates.gd
+++ i-(don't)-want-to-belive/scenes/ui/coordinates.gd
@@ -36,28 +36,10 @@ func _process(_delta):
 
 	if current_tile != last_player_tile:
 		last_player_tile = current_tile
-		update_sector_labels(current_tile)
+		update_sector_labels()
 
 
-func update_sector_labels(tile_coords: Vector2i):
-	var current_x = clampi(tile_coords.x, MapSettings.min_position.x, MapSettings.max_position.x)
-	var current_y = clampi(tile_coords.y, MapSettings.min_position.y, MapSettings.max_position.y)
-
-	if letter_label:
-		var sector_x_idx = current_x / SECTOR_TILE_SIZE
-		var letter = get_column_name(sector_x_idx)
-		letter_label.text = letter
-
-	if number_label:
-		var shifted_y = current_y - MapSettings.min_position.y
-		var row_number = (shifted_y / SECTOR_TILE_SIZE) + 1
-		number_label.text = str(row_number)
-
-
-func get_column_name(col_idx: int) -> String:
-	var name := ""
-	var temp = col_idx
-	while temp >= 0:
-		name = char(65 + (temp % 26)) + name
-		temp = (temp / 26) - 1
-	return name
+func update_sector_labels():
+	var player_position: Player.PlayerPosition = target_player.get_coordinates(target_player.global_position)
+	letter_label.text = player_position.letter
+	number_label.text = str(player_position.number)
