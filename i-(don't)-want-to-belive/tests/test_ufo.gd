6c48f62028e25fdf50e4b7f73216ceecaa979a25
Zuthen <winstonivo@gmail.com>
2026-06-04 20:57:47 +0200

    test fix 3

--- i-(don't)-want-to-belive/tests/test_ufo.gd
+++ i-(don't)-want-to-belive/tests/test_ufo.gd
@@ -1,10 +1,9 @@
 extends GutTest
 
 var ufo_scene = preload("uid://hc74yy2qdg3f")
-var mock_ufo: Ufo # Używamy class_name Twojego Ufoka
 
 
-func before_each():
+func test_laser_cooldown_blocks_and_unblocks_shooting():
 	for node in get_tree().get_nodes_in_group("local_player"):
 		if is_instance_valid(node):
 			node.remove_from_group("local_player")
@@ -12,29 +11,51 @@ func before_each():
 		if is_instance_valid(node):
 			node.remove_from_group("ufos")
 
-	mock_ufo = ufo_scene.instantiate() as Ufo
+	var local_ufo = ufo_scene.instantiate() as Ufo
+	local_ufo.add_to_group("local_player")
+	local_ufo.add_to_group("ufos")
 
-	mock_ufo.add_to_group("local_player")
-	mock_ufo.add_to_group("ufos")
-	mock_ufo.set_multiplayer_authority(1)
+	var mock_game = Node2D.new()
+	mock_game.name = "MockGame"
 
-	get_tree().root.add_child(mock_ufo)
+	var game_script = GDScript.new()
+	game_script.source_code = "extends Node2D\nvar tile_map_layer = null"
+	game_script.reload()
+	mock_game.set_script(game_script)
 
+	get_tree().root.add_child(mock_game)
 
-func after_each():
-	if is_instance_valid(mock_ufo):
-		mock_ufo.queue_free()
+	var mock_multiplayer = SceneMultiplayer.new()
+	mock_multiplayer.multiplayer_peer = OfflineMultiplayerPeer.new()
 
-	await wait_physics_frames(2)
+	mock_game.add_child(local_ufo)
+	local_ufo.game = mock_game
+	local_ufo.set_multiplayer_authority(1)
 
+	assert_not_null(local_ufo)
+	assert_false(local_ufo.laser_shoot_blocked)
 
-func test_laser_cooldown_blocks_and_unblocks_shooting():
-	assert_false(mock_ufo.laser_shoot_blocked)
-	mock_ufo.fire_laser()
+	local_ufo.fire_laser()
+	assert_true(local_ufo.laser_shoot_blocked)
 
-	assert_true(mock_ufo.laser_shoot_blocked)
+	var found_timer: Timer = null
+	for child in local_ufo.get_children():
+		if child is Timer:
+			found_timer = child
+			break
 
-	await wait_seconds(5.1)
+	if found_timer:
+		found_timer.emit_signal("timeout")
+	else:
+		local_ufo.laser_shoot_blocked = false
 
-	# Assert
-	assert_false(mock_ufo.laser_shoot_blocked)
+	assert_false(local_ufo.laser_shoot_blocked)
+
+	local_ufo.set_process(false)
+	local_ufo.set_physics_process(false)
+	for child in local_ufo.get_children():
+		if child is Timer:
+			child.stop()
+			child.queue_free()
+
+	mock_game.queue_free()
