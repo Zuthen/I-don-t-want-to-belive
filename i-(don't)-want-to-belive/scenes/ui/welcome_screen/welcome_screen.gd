b5f4b62f0536c5bc5a384048bdf6f21f5f657ee3
Zuthen <winstonivo@gmail.com>
2026-06-17 17:19:04 +0200

    quit game on main screen

--- i-(don't)-want-to-belive/scenes/ui/welcome_screen/welcome_screen.gd
+++ i-(don't)-want-to-belive/scenes/ui/welcome_screen/welcome_screen.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var room_popup = $CanvasLayer/RoomPopup
 @onready var quick_game = $CanvasLayer/Buttons/QuickGame
 @onready var buttons = $CanvasLayer/Buttons
+@onready var quit_button = $CanvasLayer/QuitButton
 
 
 func _ready():
@@ -12,15 +13,24 @@ func _ready():
 		buttons.visible = true
 	else:
 		buttons.visible = false
-	create_game.pressed.connect(_create_game_and_go_to_lobby)
-	join.pressed.connect(_show_popup)
-	quick_game.pressed.connect(_join_existing_game)
+	connect_buttons()
 	buttons.visible = false
 	NakamaNetworkManager.connection_established.connect(_on_connect)
 
 
+func connect_buttons():
+	create_game.pressed.connect(_create_game_and_go_to_lobby)
+	join.pressed.connect(_show_popup)
+	quick_game.pressed.connect(_join_existing_game)
+	quit_button.pressed.connect(_quit)
+
+
 func _on_connect():
 	buttons.visible = true
+
+
+func _quit():
+	get_tree().quit()
 
 
 func _show_popup():
