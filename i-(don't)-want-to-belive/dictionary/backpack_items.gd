63eabf76b73081ef43327e26e0f81e9eb1ae95d3
Zuthen <winstonivo@gmail.com>
2026-07-19 02:31:59 +0200

    prettier tooltips

--- i-(don't)-want-to-belive/dictionary/backpack_items.gd
+++ i-(don't)-want-to-belive/dictionary/backpack_items.gd
@@ -6,9 +6,11 @@ static func get_item_description(role: Player.Role, item_name: String) -> String
 	if role == Player.Role.ALIEN:
 		match item_name:
 			"repair_tool":
-				return "Użyj do naprawy swojego UFO"
+				return "Wichajster:
+					 Użyj do naprawy swojego UFO"
 	elif role == Player.Role.SKEPTIC:
 		match item_name:
 			"sanity_pills":
-				return "Użyj aby stracić punkt wiary"
+				return "Medi-Sinet: 
+					Użyj aby stracić punkt wiary"
 	return "Nie możesz użyć tego przedmiotu"
